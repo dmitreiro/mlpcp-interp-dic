9bcc92091f9c2bd5989a3ca5d5ed08e81f53fa7f
Dário Mitreiro <dario.mitreiro@ua.pt>
2025-10-13 17:13:18 +0100

    matchid_data_setup: script deleted, not needed

--- tools/matchid_data_setup.py
+++
@@ -1,8 +0,0 @@
-def main():
-    # check for all *nodes.csv files in that folder
-    # create a folder with parameters for each readed *nodes.csv file
-    # save each row of each file in a separate file, inside folder
-    pass
-
-if __name__ == "__main__":
-    exit(main())
